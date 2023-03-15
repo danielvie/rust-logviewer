2fc9c1d0961dd840c5321a4e70687eace2b74e70
Daniel Vieira <daniel.vieira@thermofisher.com>
2023-03-15 11:32:57 +0100

    return rawhtml content provider

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use] extern crate rocket;
 
-use rocket::{response::content, serde::Serialize, serde::json::Json};
+use rocket::{response::content, serde::Serialize, serde::json::Json, time::Instant, tokio::fs::read_to_string, futures::TryFutureExt};
 
 use std::{
     fs::{self, File},
@@ -96,6 +96,12 @@ impl Resp {
             ..Self::default()
         }
     }
+}
+
+#[get("/")]
+fn index() -> content::RawHtml<&'static str> {
+    let index = include_str!("..\\web\\index.html");
+    content::RawHtml(index)
 }
 
 #[get("/log/<nro>")]
@@ -256,41 +262,13 @@ fn launch_browser(url: &str) -> io::Result<()> {
     Ok(())
 }
 
-// #[rocket::main]
-// async fn main() -> Result<(), rocket::Error> {
-//     if let Err(e) = launch_browser("http://127.0.0.1:8000/") {
-//         eprintln!("Failed to launch browser: {}", e);
-//     }
-
-//     let _rocket = rocket::build().mount("/", routes![index, get_log, get_logs]).launch().await?;
-
-//     Ok(())
-// }
-
-
-
-use rocket::fs::{FileServer, relative};
-
-// If we wanted or needed to serve files manually, we'd use `NamedFile`. Always
-// prefer to use `FileServer`!
-mod manual {
-    use std::path::{PathBuf, Path};
-    use rocket::fs::NamedFile;
-
-    #[rocket::get("/second/<path..>")]
-    pub async fn second(path: PathBuf) -> Option<NamedFile> {
-        let mut path = Path::new(super::relative!("static")).join(path);
-        if path.is_dir() {
-            path.push("index.html");
-        }
-
-        NamedFile::open(path).await.ok()
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    if let Err(e) = launch_browser("http://127.0.0.1:8000/") {
+        eprintln!("Failed to launch browser: {}", e);
     }
-}
 
-#[rocket::launch]
-fn rocket() -> _ {
-    rocket::build()
-        .mount("/", rocket::routes![manual::second, get_logs, get_log])
-        .mount("/", FileServer::from(relative!("static")))
+    let _rocket = rocket::build().mount("/", routes![index, get_log, get_logs]).launch().await?;
+
+    Ok(())
 }
